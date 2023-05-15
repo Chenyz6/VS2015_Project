f5757a1eb3551b0a92cdb37ed15b8068406e585b
Ze <1079903036@qq.com>
2023-05-15 12:40:53 +0800

    1

--- Practice/Practice/2023.1.31.cpp
+++ Practice/Practice/2023.1.31.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-
-int main()
-{
-	int arr[] = { 99, 15, 100, 888, 252 ,4,5,6,7,8,3,2 };
-	int *p = arr;
-	cout << p[0] << endl;
-	cout << p[1] << endl;
-	cout << p[2] << endl;
-	p += 3;
-	cout << p[0];
-
-	system("pause");
-	return 0;
-}
-
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int arr[] = { 99, 15, 100, 888, 252 ,4,5,6,7,8,3,2 };
+//	int *p = arr;
+//	cout << p[0] << endl;
+//	cout << p[1] << endl;
+//	cout << p[2] << endl;
+//	p += 3;
+//	cout << p[0];
+//
+//	system("pause");
+//	return 0;
+//}
+//
---
+++ Practice/Practice/2023.5.7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//string str = "\1";
+	cout << "\1";
+	system("pause");
+}
