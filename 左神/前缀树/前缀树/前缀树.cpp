81410e7b2b6c1011f92bd3b2947b94f98dfd183f
cyz <1079903036@qq.com>
2023-05-31 17:21:08 +0800

    2023.05.31

--- 左神/前缀树/前缀树/前缀树.cpp
+++ 左神/前缀树/前缀树/前缀树.cpp
@@ -8,7 +8,7 @@ class trieNode  // trie前缀树
 public:
 	int pass; // 此节点通过了多少次
 	int end;  // 判断此字符串是不是结尾结点（是的话数值代表是多少个字符串的结尾节点）
-	trieNode * nexts[26];
+	trieNode * nexts[26];  // 如果字符种类特别多 就用哈希表
 	trieNode()
 	{
 		pass = 0;
@@ -30,7 +30,6 @@ public:
 	trie()
 	{
 		root = new trieNode;
-
 	}
 
 	void insert(string str)
@@ -52,7 +51,78 @@ public:
 			node = node->nexts[index];
 			node->pass++;
 		}
+		cout << "字符" << str << "添加成功" << endl;
 		node->end++;
+	}
+
+	int search(string str)  // 查找字符加入过几次
+	{
+		if (str.empty())
+		{
+			return 0;
+		}
+		trieNode * node = root;
+		int index = 0;
+		for (int i = 0; i < str.length(); i++)
+		{
+			index = str[i] - 'a';
+			if (node->nexts[index] == NULL)
+			{
+				return 0;
+			}
+			node = node->nexts[index];
+		}
+		return node->end;
+	}
+
+	int prefixNumber(string str) // 前缀出现次数
+	{
+		if (str.empty())
+		{
+			return 0;
+		}
+		trieNode * node = root;
+		int index = 0;
+		for (int i = 0; i < str.length(); i++)
+		{
+			index = str[i] - 'a';
+			if (node->nexts[index] == NULL)
+			{
+				return 0;
+			}
+			node = node->nexts[index];
+		}
+		return node->pass;
+	}
+
+	void deleteStr(string str)
+	{
+		if (search(str) != 0)
+		{
+			trieNode * node = root;
+			int index = 0;
+			node->pass--;
+			for (int i = 0; i < str.length(); i++)
+			{
+				index = str[i] - 'a';
+				if (--node->nexts[index]->pass == 0)
+				{
+					delete node->nexts[index];
+					node->nexts[index] = NULL;  // 遍历删除之后所有结点
+					for (int i = 0; i < 26; i++)
+					{
+						if (node->nexts[i] != NULL)
+						{
+							delete node->nexts[i];
+							node->nexts[i] = NULL;
+						}
+					}
+					return;
+				}
+				node = node->nexts[index];
+			}
+			node->end--;
+		}
 	}
 };
 
@@ -61,7 +131,18 @@ int main()
 	trie t;
 	string s = "abc";
 	string s1 = "abcd";
+	string s2 = "ab";
+	string s3 = "ab";
+	string s4 = "abc";
 	t.insert(s);
 	t.insert(s1);
+	t.insert(s2);
+	t.insert(s3);
+	t.insert(s4);
+	cout << "-----------------" << endl;
+	cout << "abc出现过" << t.search("abc") << "次" << endl;
+	cout << "ab为前缀出现过" << t.prefixNumber("ab") << "次" << endl;
+	t.deleteStr("ab");
+	cout << "ab出现过" << t.search("ab") << "次" << endl;
 	system("pause");
 }
